e6fa24e507d51b03c272d81658731f7a348b5b47
jr0joker69 <jr0joker69@gmail.com>
2026-04-02 09:39:38 +0000

    fix: --bind auto for Render

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p /opt/render/.openclaw
 cp ./openclaw.json /opt/render/.openclaw/openclaw.json
-openclaw gateway run --allow-unconfigured
+openclaw gateway run --allow-unconfigured --bind auto
