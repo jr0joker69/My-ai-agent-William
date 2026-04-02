cd3d4d47f9923772241c598a177fa7e3ff5eec44
jr0joker69 <jr0joker69@gmail.com>
2026-04-02 11:02:08 +0000

    fix: add HTTP server to satisfy Render port scan

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 mkdir -p /opt/render/.openclaw
 cp ./openclaw.json /opt/render/.openclaw/openclaw.json
+node -e "require('http').createServer((req,res)=>res.end('OK')).listen(process.env.PORT||3000)" &
 openclaw gateway run --allow-unconfigured --bind auto
